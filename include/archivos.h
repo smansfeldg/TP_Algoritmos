fe145dbf3727eb2aa65abd1cadd2272ed409055a
Matias <matiasespinoza100@hotmail.com>
2026-04-21 18:30:55 -0300

    Agregue las funciones para cerrar y abrir archivo.

--- include/archivos.h
+++ include/archivos.h
@@ -1,13 +1,22 @@
 #ifndef ARCHIVOS_H
 #define ARCHIVOS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * @file archivos.h
  * @brief Manejo de persistencia y lectura de archivos.
  *
  * Capa de Acceso a Datos.
- * Gestión de lectura de config.txt, caravana.txt y la 
+ * Gestión de lectura de config.txt, caravana.txt y la
  * persistencia binaria de los usuarios registrados.
  */
+
+
+int abrirArchivo(FILE ** arch, const char *nombreArchivo, const char *modoApertura, int mostrarError);
+int cerrarArchivo(FILE* arch);
+
+
 
 #endif // ARCHIVOS_H
--- src/archivos.c
+++ src/archivos.c
@@ -2,8 +2,29 @@
  * @file archivos.c
  * @brief Implementación de las operaciones de entrada/salida de archivos.
  *
- * Funciones para cargar configuraciones, mapas y guardar 
+ * Funciones para cargar configuraciones, mapas y guardar
  * el estado y usuarios de forma persistente.
  */
 
 #include "../include/archivos.h"
+
+int abrirArchivo(FILE ** arch, const char *nombreArchivo, const char *modoApertura, int mostrarError)
+{
+    *arch = fopen(nombreArchivo,modoApertura);
+
+    if(*arch==NULL)
+    {
+        if(mostrarError==1)
+            printf("Error al abrir el archivo %s en modo %s.", nombreArchivo,modoApertura);
+        return 0;
+    }
+    return 1;
+}
+int cerrarArchivo(FILE* arch)
+{
+    if(fclose(arch)==0)
+        return 1; //Cerrado Correctamente
+    else
+        return 0;
+
+}
