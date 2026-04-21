18caf716551e1f7442fe69480f51a4c9189eb699
smansfeldg <santimansfeld@gmail.com>
2026-04-20 21:34:42 -0300

    feat: implement basic operations for circular doubly linked list

--- src/lista_circular.c
+++ src/lista_circular.c
@@ -6,3 +6,71 @@
  */
 
 #include "../include/lista_circular.h"
+
+void crearLista(tLista *p)
+{
+    
+}
+
+int vaciarLista(tLista *p)
+{
+    
+    return 0;
+}
+
+int listaVacia(const tLista *p)
+{
+    
+    return 0;
+}
+
+int listaLlena(const tLista *p, unsigned cantBytes)
+{
+    
+    return 0;
+}
+
+int insertarAlFinal(tLista *p, const void *d, unsigned cantBytes)
+{
+    
+    return 0;
+}
+
+int insertarAlComienzo(tLista *p, const void *d, unsigned cantBytes)
+{
+    
+    return 0;
+}
+
+void mostrarDeIzqADer(const tLista *p,
+                      void (*mostrar)(const void *))
+{
+    
+}
+
+void mostrarDeDerAIzq(const tLista *p,
+                      void (*mostrar)(const void *))
+{
+    
+}
+
+int insertarEnOrden(tLista *p, const void *d, unsigned cantBytes,
+                    int (*comparar)(const void *, const void *),
+                    int (*acumular)(void **, unsigned *,
+                                     const void *, unsigned))
+{
+    
+    return 0;
+}
+
+void ordenarLista(tLista *p, int (*comparar)(const void *, const void *))
+{
+    
+}
+
+int eliminarPorClave(tLista *p, void *d, unsigned cantBytes,
+                     int (*comparar)(const void *, const void *))
+{
+    
+    return 0;
+}
