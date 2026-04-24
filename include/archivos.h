07245b23ad00e8fa80385b42776d6f8770631f08
Matias <matiasespinoza100@hotmail.com>
2026-04-24 00:03:09 -0300

    Feat: Added functions to read files (.txt and .bin). Added function to create a bin test files and others to show .txt and .bin files.

--- include/archivos.h
+++ include/archivos.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAM_LINEA 30
+#define TAM 20
+
 /**
  * @file archivos.h
  * @brief Manejo de persistencia y lectura de archivos.
@@ -14,11 +17,26 @@
  */
 
 
+//Utilizado como Prueba
+typedef struct{
+    char nombre[TAM];
+    int puntos;
+    int cantPartidas;
+}sPrueba;
+
+typedef void (*accion) (void *a);
+
+//Funciones para mostrar archivos creadas como prueba
+void mostrarTxt(void *dato);
+void mostrarBin(void *dato);
+
 int abrirArchivo(FILE ** arch, const char *nombreArchivo, const char *modoApertura, int mostrarError);
 int cerrarArchivo(FILE ** arch, const char *nombreArchivo, int mostrarError);
 
+int recorrerArchivoTxt(FILE* arch, const char *nombreArchivo, accion Accion);
+int recorrerArchivoBin(FILE* arch, const char *nombreArchivo, unsigned tam, accion Accion);
 
-
-
+//Crea un archivo de prueba para el binario
+void crearPrueba(const char *nombreArchivo);
 
 #endif // ARCHIVOS_H
--- src/archivos.c
+++ src/archivos.c
@@ -31,3 +31,65 @@ int cerrarArchivo(FILE ** arch, const char *nombreArchivo, int mostrarError)
     return 0;
 
 }
+
+int recorrerArchivoTxt(FILE* arch, const char *nombreArchivo, accion Accion)
+{
+    char linea[TAM_LINEA];
+    while(fgets(linea,TAM_LINEA-1,arch))
+    {
+        Accion(linea);
+    }
+
+    return 1;
+}
+
+int recorrerArchivoBin(FILE* arch, const char *nombreArchivo, unsigned tam, accion Accion)
+{
+    void *aux;
+    aux=malloc(tam);
+    if(!aux)
+        return 0;
+    fread(aux,tam,1,arch);
+    while(!feof(arch))
+
+    {
+        Accion(aux);
+        fread(aux,tam,1,arch);
+    }
+
+    free(aux);
+
+    return 1;
+}
+
+void mostrarLinea(void *dato)
+{
+    char *d = (char*)dato;
+    printf("%s\n", d);
+}
+
+void mostrarBin(void *dato)
+{
+    sPrueba *d = (sPrueba*)dato;
+    printf("%s|%d|%d\n", d->nombre,d->puntos,d->cantPartidas);
+}
+
+void crearPrueba(const char *nombreArchivo)
+{
+    FILE* arch;
+    abrirArchivo(&arch,"prueba.bin","wb",1);
+    sPrueba jugadores[]={{"Juan Perez",4,0},
+                      {"Florencia Re",4,0},
+                      {"Maria Benitez",7,5},
+                      {"Victor Gomez",7,5},
+                      {"Flavio Gomez",9,2},
+                      {"Julieta Beltran",3,2},
+                      {"Fernanda Gauna",9,1},
+                      {"Lucas Grance",10,2},
+                      {"Pablo Soligo",10,3}};
+    unsigned tamanio = sizeof(sPrueba);
+    int ce = sizeof(jugadores)/tamanio;
+
+    fwrite(jugadores, sizeof(sPrueba), ce, arch);
+    cerrarArchivo(&arch,"prueba.bin",1);
+}
