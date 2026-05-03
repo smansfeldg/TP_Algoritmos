cac01cde8cd21861978217e378584b5bf6125bae
Matias <matiasespinoza100@hotmail.com>
2026-05-03 19:24:36 -0300

    Fix: Eliminated unnecessary functions, leaving only the generic ones, and revised the other.

--- include/archivos.h
+++ include/archivos.h
@@ -7,10 +7,8 @@
 
 #define ERROR_ARCH 0
 #define ERROR_MEM -1
-#define NO_ENCONTADO -1
-
-#define TAM_LINEA 30
-#define TAM 26
+#define NO_ENCONTRADO -1
+#define OK 1
 
 /**
  * @file archivos.h
@@ -21,71 +19,22 @@
  * persistencia binaria de los usuarios registrados.
  */
 
-
-typedef struct{
-    char nombre[TAM];
-    int puntosTotales;
-    int cantPartidas;
-}sJugador;
-
-typedef struct{
-    char nombeJugador[TAM];
-    int puntos;
-    int movimientos;
-}sPartida;
-
-typedef struct{
-    int cantPos;
-    int vidasIniciales;
-    int maxBandidos;
-    int maxPremios;
-    int maxVidasExtra;
-    int maxOasis;
-    int maxTormentas;
-}sConfig;
-
-typedef struct{
-    int pos;
-    char objetos[TAM];
-}sCasilla;
-
 typedef void (*accion) (void *b,const void *a);
 typedef int (*cmp) (const void *a, const void *b);
 
-////Funciones Genericas de Archivos
 int abrirArchivo(FILE ** arch, const char *nombreArchivo, const char *modoApertura, int mostrarError);
 int cerrarArchivo(FILE ** arch, const char *nombreArchivo, int mostrarError);
 
-
-int mostrarArchivoTxt(FILE* arch, const char *nombreArchivo, accion Accion);
-int leerArchivoTxt(FILE* arch, void *estDatos, unsigned tam, accion Accion, accion Cargar);
-
-int mostrarArchivoBin(FILE* arch, unsigned tam, accion Accion);
+int leerArchivoTxt(FILE* arch, void *estDatos, unsigned tam, unsigned tamLinea, accion Accion, accion Cargar);
 int leerArchivoBin(FILE* arch, void *estDatos, unsigned tam, accion Accion, accion Cargar);
 
-////Funciones para manejo de registros en Archivos Binarios
-int escribirRegistro(FILE* arch,const void *dato, unsigned tam);
-int leerRegistro(FILE* arch, void *dato, unsigned tam);
-
-int buscarRegistro(FILE* arch,const void *dato, unsigned tam, cmp Cmp);//Si encuentra registro, posiciona el puntero a principio de este
-
+int buscarRegistro(FILE* arch, void *reg,const void *dato, unsigned tam, cmp Cmp);
 int leerPos(FILE* arch, void *dato, unsigned tam, int pos);
 int escribirPos(FILE* arch,const void *dato, unsigned tam, int pos);
 
-int actualizarRegistro(FILE* arch, void *dato, unsigned tam, accion Accion);
+int actualizarRegistro(FILE* arch, void *reg, const void *dato, unsigned tam, accion Accion);
 
-int cmpNombre(const void *a, const void *b);
-////Trozado
-void trozarConfig(void *config, const void *dato);
-void trozarMapa(void *mapa, const void *dato);
-void trozarJugador(void *jugador, const void *dato);
-void trozarPartida(void *partida, const void *dato);
+int escribirTxt(FILE *arch, void *dato, unsigned tamLinea, accion Accion);
 
-////Cargar el registro en la estructure de datos que corresponda;
-void cargarLista(void *lista, const void *dato);
-
-
-void mostrarLinea(void *a,const void *dato);
-void mostrarJugador(void *a,const void *dato);
 
 #endif // ARCHIVOS_H
--- src/archivos.c
+++ src/archivos.c
@@ -8,7 +8,6 @@
 
 #include "../include/archivos.h"
 
-/////////////////////
 int abrirArchivo(FILE ** arch, const char *nombreArchivo, const char *modoApertura, int mostrarError)
 {
     *arch = fopen(nombreArchivo,modoApertura);
@@ -19,12 +18,12 @@ int abrirArchivo(FILE ** arch, const char *nombreArchivo, const char *modoApertu
             fprintf(stderr,"Error al abrir el archivo %s en modo %s.\n", nombreArchivo,modoApertura);
         return ERROR_ARCH;
     }
-    return 1;
+    return OK;
 }
 int cerrarArchivo(FILE ** arch, const char *nombreArchivo, int mostrarError)
 {
     if(fclose(*arch)==0)
-        return 1; //Cerrado Correctamente
+        return OK;
 
     else if(mostrarError==1)
         fprintf(stderr,"Error al cerrar el archivo %s.\n", nombreArchivo);
@@ -33,71 +32,48 @@ int cerrarArchivo(FILE ** arch, const char *nombreArchivo, int mostrarError)
 
 }
 
-int mostrarArchivoTxt(FILE* arch, const char *nombreArchivo, accion Accion)
-{
-    char linea[TAM_LINEA];
-
-    rewind(arch);
-
-    fgets(linea,TAM_LINEA-1,arch);
-    while(!feof(arch))
-    {
-        Accion(NULL, linea);
-        fgets(linea,TAM_LINEA-1,arch);
-    }
-
-    return 1;
-}
-
-int leerArchivoTxt(FILE* arch, void *estDatos, unsigned tam, accion Accion, accion Cargar)
+/////////////////////
+/////////////////////
+int leerArchivoTxt(FILE* arch, void *estDatos, unsigned tam, unsigned tamLinea, accion Accion, accion Cargar)
 {
     void *aux = malloc(tam);
-    if(!aux)
+    if(aux==NULL)
         return ERROR_MEM;
 
-    char linea[TAM_LINEA];
+    char *linea=malloc(tamLinea);
+    if(linea==NULL)
+    {
+        free(aux);
+        return ERROR_MEM;
+    }
 
     rewind(arch);
 
-    fgets(linea,TAM_LINEA-1,arch);
+    fgets(linea,tamLinea,arch);
     while(!feof(arch))
     {
+        //Trozar linea y cargarla en aux.
         Accion(aux, linea);
+        //Cargar aux en la estructura de datos correspondiente.
         Cargar(estDatos, aux);
-        fgets(linea,TAM_LINEA-1,arch);
+        fgets(linea,tamLinea,arch);
     }
 
+    free(linea);
     free(aux);
-    return 1;
+    return OK;
 }
 
-int mostrarArchivoBin(FILE* arch, unsigned tam, accion Accion)
-{
-    void *aux = malloc(tam);
-    if(!aux)
-        return ERROR_MEM;
-
-    rewind(arch);
-
-    fread(aux,tam,1,arch);
-    while(!feof(arch))
-    {
-        Accion(NULL, aux);
-        fread(aux,tam,1,arch);
-    }
-
-    free(aux);
-    return 1;
-}
-
+/////////////////////
+/////////////////////
 int leerArchivoBin(FILE* arch, void *estDatos, unsigned tam, accion Accion, accion Cargar)
 {
     void *reg=malloc(tam);
-    if(!reg)
+    if(reg==NULL)
         return ERROR_MEM;
 
-    void *dato = malloc(tam);
-    if(!dato)
+    void *dato=malloc(tam);
+    if(dato==NULL)
     {
         free(reg);
         return ERROR_MEM;
@@ -109,40 +85,39 @@ int leerArchivoBin(FILE* arch, void *estDatos, unsigned tam, accion Accion, acci
     while(!feof(arch))
 
     {
+        //Trozar registro y cargarlo en dato.
         Accion(dato, reg);
+        //Cargar dato en la estructura de datos correspondiente.
         Cargar(estDatos, dato);
         fread(reg,tam,1,arch);
     }
 
     free(reg);
     free(dato);
-    return 1;
+    return OK;
 }
 
-////
-////
-
-int escribirRegistro(FILE* arch,const void *dato, unsigned tam)
+/////////////////////
+/////////////////////
+int buscarRegistro(FILE* arch, void *reg, const void *dato, unsigned tam, cmp Cmp)
 {
-    return fwrite(dato, tam,1,arch)==1;
-}
-
-
-    //Busca el registro, Si lo encuentra, se posiciona al principio
-int buscarRegistro(FILE* arch,const void *dato, unsigned tam, cmp Cmp)
-{
-    rewind(arch);
     void *aux=malloc(tam);
     if(aux==NULL)
         return ERROR_MEM;
+
     int pos=0;
 
+    rewind(arch);
+
+    //Busca el registro con un contador para la posicion.
     fread(aux, tam, 1, arch);
     while (!feof(arch))
     {
         if (Cmp(aux, dato) == 0)
         {
+            //Carga el registro en *reg y devuelve su posicion.
             fseek(arch,-tam,SEEK_CUR);
+            memcpy(reg,aux,tam);
             free(aux);
             return pos;
         }
@@ -151,129 +126,53 @@ int buscarRegistro(FILE* arch,const void *dato, unsigned tam, cmp Cmp)
     }
 
     free(aux);
-    return NO_ENCONTADO;
+    //Si no se encuentra, devuelve -1 y ep puntero queda al final.
+    return NO_ENCONTRADO;
 }
 
-int leerRegistro(FILE* arch, void *dato, unsigned tam)
+int actualizarRegistro(FILE* arch, void *reg, const void *dato, unsigned tam, accion Accion)
 {
-    return fread(dato,tam,1,arch)==1;
-}
+    //Reg es obtenido del archivo y dato es conseguido durante la partida
+    //Actualiza el registro a partir del dato.
+    Accion(reg,dato);
 
-int actualizarRegistro(FILE* arch, void *dato, unsigned tam, accion Accion)
-{
-    void *aux=malloc(tam);
-    if(aux==NULL)
-        return ERROR_MEM;
-
-    leerRegistro(arch,dato,tam);
-
-    fseek(arch,-tam,SEEK_CUR);
-
-    Accion(aux,dato);
-
-    escribirRegistro(arch,aux,tam);
-
-    free(aux);
-    return 1;
+    return fwrite(dato,tam,1,arch)==1;
 }
 
 int leerPos(FILE* arch, void *dato, unsigned tam, int pos)
 {
-    fseek(arch,tam*pos,SEEK_SET);
+    if(fseek(arch,tam*pos,SEEK_SET)!=0)
+        return ERROR_ARCH;
 
     return fread(dato,tam,1,arch)==1;
 }
 
 int escribirPos(FILE* arch,const void *dato, unsigned tam, int pos)
 {
-    fseek(arch,tam*pos,SEEK_SET);
+    if(fseek(arch,tam*pos,SEEK_SET)!=0)
+        return ERROR_ARCH;
 
     return fwrite(dato,tam,1,arch)==1;
 }
 
-//////
-//////
-
-int cmpNombre(const void *a, const void *b)
+/////////////////////
+/////////////////////
+int escribirTxt(FILE *arch, void *dato, unsigned tamLinea, accion Accion)
 {
-    sJugador *j1 = (sJugador*)a;
-    sJugador *j2 = (sJugador*)b;
+    char *linea=malloc(tamLinea);
+    if(linea==NULL)
+        return ERROR_MEM;
 
-    return strcmpi(j1->nombre,j2->nombre);
-}
+    //Da formato al dato y lo carga en un string.
+    Accion(linea,dato);
 
-void trozarConfig(void *config, const void *dato)
-{
-    char *linea =(char*) dato;
-    sConfig *c = (sConfig*) config;
-    char parametro[TAM];
-    int valor;
-    sscanf(linea," %26[^: ] %*[: ] %d",parametro,&valor);
+    //Escribe en el archivo lo que este escrito en linea.
+    if(fprintf(arch,"%s\n",linea)<0)
+    {
+        free(linea);
+        return ERROR_ARCH;
+    }
 
-    if(strcmpi(parametro,"cantidad_posiciones")==0)
-        c->cantPos=valor;
-
-    else if(strcmpi(parametro,"vidas_inicio")==0)
-        c->vidasIniciales=valor;
-
-    else if(strcmpi(parametro,"maximo_bandidos")==0)
-        c->maxBandidos=valor;
-
-    else if(strcmpi(parametro,"maximo_premios")==0)
-        c->maxPremios=valor;
-
-    else if(strcmpi(parametro,"maximo_vidas_extra")==0)
-        c->maxVidasExtra=valor;
-
-    else if(strcmpi(parametro,"maximo_oasis")==0)
-        c->maxOasis=valor;
-
-    else if(strcmpi(parametro,"maximo_tormentas")==0)
-        c->maxTormentas=valor;
-}
-
-void trozarMapa(void *mapa, const void *dato)
-{
-    char *linea =(char*) dato;
-    sCasilla *c = (sCasilla*) mapa;
-    sscanf(linea,"%d:%s",&c->pos,c->objetos);
-}
-
-void trozarJugador(void *jugador,const void *dato)
-{
-    sJugador *d = (sJugador*)dato;
-    sJugador *j = (sJugador*)jugador;
-
-    j->cantPartidas=d->cantPartidas;
-    j->puntosTotales=d->puntosTotales;
-    strcpy(j->nombre,d->nombre);
-}
-
-
-void trozarPartida(void *partida,const void *dato)
-{
-    sPartida *d = (sPartida*)dato;
-    sPartida *p =(sPartida*)partida;
-
-    p->movimientos=d->movimientos;
-    strcpy(p->nombeJugador,d->nombeJugador);
-    p->puntos=d->puntos;
-}
-
-
-void cargarLista(void *lista, const void *dato)
-{
-
-}
-
-void mostrarLinea(void *a,const void *dato)
-{
-    char *d = (char*)dato;
-    printf("%s\n", d);
-}
-
-void mostrarJugador(void *a ,const void *dato)
-{
-    sJugador *d = (sJugador*)dato;
-    printf("%s|%d|%d\n", d->nombre,d->puntosTotales,d->cantPartidas);
+    free(linea);
+    return OK;
 }
